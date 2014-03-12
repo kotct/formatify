e460148c9f44dbd3a12c8e342e00ec45928651be
Kristofer Rye <kristofer.rye@gmail.com>
2014-03-12 10:57:35 -0400

    Including some getopt-y stuff.

--- formatify.sh
+++ formatify.sh
@@ -1,1 +1,21 @@
 #!/bin/sh
+
+while getopts a:b:c flag;
+do
+	case $flag in
+		a)
+			echo "-a used: $OPTARG";
+			;;
+		b)
+			echo "-b used: $OPTARG";
+			;;
+		c)
+			echo "-c used";
+			;;
+		?)
+			exit;
+			;;
+	esac
+done
+
+shift $((OPTIND - 1))
